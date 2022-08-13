0b0b9b7f6528ce91651087e9135e3e5452d8e996
dag-bag <virenderkumar23435@gmail.com>
2022-08-13 20:15:23 +0530

    New update

--- components/home/Mission.js
+++ components/home/Mission.js
@@ -6,8 +6,12 @@ function Mission({ title, description, image, boxType }) {
   return (
     <div className="flex  justify-evenly flex-wrap-reverse py-16 ">
       <div className="px-5 flex justify-end flex-col  space-y-12 mt-8">
-        <div>
-          <p className="relative  text-2xl  lg:text-4xl text-right text-[#333]">
+        <div
+          className={` ${
+            boxType === "center-left" ? "text-left" : "text-right"
+          }`}
+        >
+          <p className="relative  text-2xl  lg:text-4xl  text-[#333]">
             {title}
             <svg
               width={60}
@@ -17,16 +21,14 @@ function Mission({ title, description, image, boxType }) {
               xmlns="http://www.w3.org/2000/svg"
               className={`absolute  right-0 ${
                 boxType === "center-center" ? "top-18" : "top-10"
-              }`}
+              }  ${boxType === "center-left" ? "left-0" : "right-0"}`}
               preserveAspectRatio="none"
             >
               <path d="M0 1L60 0.999995" stroke="#333333" />
             </svg>
           </p>
 
-          <p className=" text-base text-right text-[#333] mt-12">
-            {description}
-          </p>
+          <p className=" text-base  text-[#333] mt-12">{description}</p>
         </div>
         {/* <div className="flex justify-end flex-col space-y-16">
          
@@ -67,7 +69,11 @@ function Mission({ title, description, image, boxType }) {
             </svg>
           </div>
         </div> */}
-        <button className="ml-auto px-14 md:px-24 py-3 md:py-2 border  border-[#bd9575]  md:text-lg font-medium text-center text-[#bd9575]">
+        <button
+          className={`${
+            boxType === "center-left" ? "mr-auto" : "ml-auto"
+          }  px-14 md:px-24 py-3 md:py-2 border  border-[#bd9575]  md:text-lg font-medium text-center text-[#bd9575]`}
+        >
           Cosmos
         </button>
       </div>
