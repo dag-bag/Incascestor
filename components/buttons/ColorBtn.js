c34292b5ae0f99d15c7a0c3c0311259754b5973c
dag-bag <virenderkumar23435@gmail.com>
2022-08-18 18:08:44 +0530

    Update please

--- components/buttons/ColorBtn.js
+++ components/buttons/ColorBtn.js
@@ -7,7 +7,11 @@ function ColorBtn({ color }) {
     <span
       className={`block p-1 border-2  border-gray-500 rounded-full transition ease-in duration-300`}
     >
-      <a className={`block w-6 h-6 bg-${color}-500  rounded-full`}></a>
+      <a
+        className={`block w-6 h-6 ${color === "white" && "bg-gray-400"} ${
+          color === "black" && "bg-black"
+        } bg-${color}-500  rounded-full `}
+      ></a>
     </span>
   );
 }
