2cbcefa8ccd654d394086a2b10426198cab6bec5
dag-bag <virenderkumar23435@gmail.com>
2022-09-05 19:58:45 +0530

    yes

--- pages/api/product.js
+++ pages/api/product.js
@@ -9,7 +9,7 @@ const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ["GET", "POST", "PUT"],
+    methods: ["POST", "GET", "PUT"],
   })
 );
 
@@ -44,10 +44,10 @@ const handler = async (req, res) => {
       await newProduct.save();
     }
 
-    res.status(200).json({ success: "Product Created SuccessFully" });
+    res.status(200).json({ success: "Product Created SuccessFully yahhh" });
   }
   if (req.method === "GET") {
-    // await cors(req, res);
+    await cors(req, res);
     let products = await Product.find();
     res.status(200).json({ products });
   }
