f9d9fb5753f78426e93a3037483bb96b2f9c9fa1
dag-bag <virenderkumar23435@gmail.com>
2022-09-16 01:39:52 +0530

    This is good

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,6 +1,17 @@
 /** @format */
-
+import initMiddleware from "../../lib/initiateMiddleware";
+import Cors from "cors";
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["POST", "GET", "PUT"],
+    origin: "*",
+    optionsSuccessStatus: 200,
+  })
+);
 export default async function handler(req, res) {
+  await cors(req, res);
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
